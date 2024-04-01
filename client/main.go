6dca497df7baa67aee4c33b9212da2c372812c72
danieljcksn <daniiel.cavallcante@gmail.com>
2024-04-01 09:56:24 -0300

    feat: add client-server grpc connection

--- client/main.go
+++ client/main.go
@@ -5,23 +5,28 @@ import (
 	"log"
 	"time"
 
-	pb "github.com/danieljcksn/connect-four/game/proto"
+	pb "github.com/danieljcksn/connect-four/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	if err != nil {
 		log.Fatalf("failed to connect to gRPC server at localhost:50051: %v", err)
 	}
+
 	defer conn.Close()
-	c := pb.NewHelloWorldServiceClient(conn)
+
+	client := pb.NewGameServiceClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
 	defer cancel()
 
-	r, err := c.SayHello(ctx, &pb.HelloWorldRequest{})
+	r, err := client.Connect(ctx, &pb.ConnectRequest{})
+
 	if err != nil {
 		log.Fatalf("error calling function SayHello: %v", err)
 	}
--- server/main.go
+++ server/main.go
@@ -5,29 +5,41 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/danieljcksn/connect-four/game/proto"
+	pb "github.com/danieljcksn/connect-four/proto"
 
 	"google.golang.org/grpc"
 )
 
-type server struct {
-	pb.UnimplementedHelloWorldServiceServer
+type gameServer struct {
+	pb.UnimplementedGameServiceServer
 }
 
-func (s *server) SayHello(ctx context.Context, in *pb.HelloWorldRequest) (*pb.HelloWorldResponse, error) {
-	return &pb.HelloWorldResponse{Message: "Hello, World! "}, nil
+func (s *gameServer) Connect(
+	ctx context.Context,
+	req *pb.ConnectRequest,
+) (*pb.ConnectResponse, error) {
+	return &pb.ConnectResponse{
+		Message: "Connected to the server!",
+	}, nil
+}
+
+func newGameServer() *gameServer {
+	s := &gameServer{}
+
+	return s
 }
 
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
+
 	if err != nil {
-		log.Fatalf("failed to listen on port 50051: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	pb.RegisterHelloWorldServiceServer(s, &server{})
-	log.Printf("gRPC server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+	grpcServer := grpc.NewServer()
+	pb.RegisterGameServiceServer(grpcServer, newGameServer())
+
+	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
